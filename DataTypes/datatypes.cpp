4d39487c6987580ed77231983a3a75391883d357
Minhaa26 <minahilabid396@gmail.com>
2026-01-15 16:59:45 +0500

    Update datatypes.cpp

--- DataTypes/datatypes.cpp
+++ DataTypes/datatypes.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 21;
+    float percentage = 87.5;
+    char grade = 'A';
+    bool isPassed = true;
+
+    cout << "Age: " << age << endl;
+    cout << "Percentage: " << percentage << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Passed: " << isPassed << endl;
+
+    return 0;
+}
 
