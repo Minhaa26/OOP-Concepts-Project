1531c653c39d48078b0031f2fdfdae951f28ffc7
Minhaa26 <minahilabid396@gmail.com>
2026-01-15 17:04:09 +0500

    Update encapsulation.cpp

--- OOP/encapsulation.cpp
+++ OOP/encapsulation.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
 
+class BankAccount {
+private:
+    int balance;
+
+public:
+    void setBalance(int b) {
+        balance = b;
+    }
+
+    int getBalance() {
+        return balance;
+    }
+};
+
+int main() {
+    BankAccount acc;
+    acc.setBalance(15000);
+
+    cout << "Balance: " << acc.getBalance() << endl;
+    return 0;
+}
