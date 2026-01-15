7842267df56210f258089efa87ea2e45e2f2c665
Minhaa26 <minahilabid396@gmail.com>
2026-01-15 17:03:13 +0500

    Update inheritance.cpp

--- OOP/inheritance.cpp
+++ OOP/inheritance.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Person {
+public:
+    string name;
+};
+
+class Student : public Person {
+public:
+    int id;
+};
+
+int main() {
+    Student s;
+    s.name = "Ahmed";
+    s.id = 10;
+
+    cout << "Name: " << s.name << endl;
+    cout << "ID: " << s.id << endl;
+
+    return 0;
+}
 
